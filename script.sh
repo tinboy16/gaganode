4ea702652df606146b9eef1474310c20d855a900
tinnguyen <108519316+tinboy16@users.noreply.github.com>
2024-05-07 01:22:29 -0700

    Update script.sh
    sudo off

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@
 curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
 
 # Step 2: Remove existing service and install new service
-sudo ./apphub service remove && sudo ./apphub service install
+./apphub service remove && sudo ./apphub service install
 
 # Step 3: Start service
-sudo ./apphub service start
+./apphub service start
 
 # Step 4: Check app status
 ./apphub status
@@ -19,7 +19,7 @@ echo "Pausing for 15 seconds..."
 sleep 15
 
 # Step 6: Set token
-sudo ./apps/gaganode/gaganode config set --token=znkivdrssydfxcwcea3383c455384139
+./apps/gaganode/gaganode config set --token=znkivdrssydfxcwcea3383c455384139
 
 # Step 7: Restart app
 ./apphub restart
